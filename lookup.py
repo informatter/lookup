dbc64a98646808ff98490cbd21f57fa98474fba2
informatter <nicholas.rawitscher@gmail.com>
2024-04-30 14:31:21 +0100

    1.Finalized v2 implementation using double hashing for collision resolution 2. Insalled pytest and added initial unit tests

--- lookup.py
+++ lookup.py
@@ -1,49 +1,111 @@
+from dataclasses import dataclass
 from typing import Any
 from typing import List
+
+
+@dataclass
+class Data:
+    def __init__(self,key:Any, value:Any,is_deleted:bool=False):
+        self.key = key
+        self.value=value
+        self.is_deleted  = is_deleted
+    def __str__(self):
+        return f'key: {self.key}, value: {self.value}, is_deleted:{self.is_deleted}'
+    def __repr__(self):
+        return str(self)
+    
+
+
 
 class Lookup:
 
     def __init__(self, length:int=10):
         self.length:int = length
-        self.buckets:List[List[dict]] = [list() for _ in range(self.length)]#[ [],[]...n]
+        self.buckets:List[Data | None] = [None for _ in range(self.length)]
         self.size = 0
+        # TODO Calculate load factor: 𝛼 = n / m, where n are total items and m is table size -> should be kept < 0.75
 
     
-    def __hash(self,key:Any)->int:
+    def __hash(self,key:Any, collision_count:int)->int:
         """
-        Hashes the key based on the Integer universe assumption using the hasing by division technique
+        Hashes the key based on the Integer universe assumption using the hashing by division technique
         """
-        return hash(key) % self.length
-    
+        key_hash = hash(key)
+        hash_1 = key_hash % self.length
+        hash_2 = 1 + (key_hash % (self.length - 1))
+        return (hash_1 + collision_count * hash_2) % self.length
 
-    def __get_bucket(self,key:Any) -> List[dict]:
-        hash:int = self.__hash(key)
-        return self.buckets[hash]
+
     
 
     def __resize(self):
         #TODO
         pass
 
-    def __handle_collisions(self):
-        #TODO
-        pass
-
-    
 
     def insert(self, key:Any, value:Any) -> None:
-        target_bucket:List[dict]  = self.__get_bucket(key)
 
-        # Does not add items with the same key to the bucket. Instead it updates the old value with the new value
-        for item in target_bucket:
-            if item["key"] == key:
-                item["value"] = value
+
+        collision_count = 0
+        home_location = self.__hash(key,collision_count)
+
+        if self.buckets[home_location] is None:
+            self.buckets[home_location] = Data(key,value)
+            self.size+=1
+            return
+
+        delta_location = None
+        collision_count+=1
+        
+        # location is full - start probing!
+
+        while home_location != delta_location:
+            delta_location = self.__hash(key,collision_count)
+            if self.buckets[delta_location] is None:
+                self.buckets[delta_location] = Data(key,value)
+                self.size+=1
                 return
-            
-        # handles hash colission: key is different but hashed key is the same.
-        # v1
-        target_bucket.append({"key":key,"value":value})
-        self.size+=1
+            collision_count+=1
+        
+        # TODO handle when insertion can't happen -> resize table is full!
+
+
+    def search (self, key:Any)-> Any | None:
+        """
+        Gets a value by a specified key.
+
+        Returns
+        -------
+        `None` if the element does not exist; Otherwise the value is returned
+        """
+
+        collision_count = 0
+        home_location = self.__hash(key,collision_count)
+        data:None|Data = self.buckets[home_location]
+
+        if data is None:
+            return None
+
+        if data is not None and data.key == key and data.is_deleted is False:
+            return data.value
+        
+    
+        delta_location = None
+        collision_count+=1
+        
+        # start probing!
+        # when load factor approaches 1 or is 1 search is O(n)
+
+        while home_location != delta_location:
+            delta_location = self.__hash(key,collision_count)
+            data:None|Data = self.buckets[delta_location]
+            if data is None:
+                return None
+            if data is not None and data.key == key and data.is_deleted is False:
+                return data.value
+            collision_count+=1
+        
+        return None
 
    
     def delete(self,key:Any)->bool:
@@ -55,45 +117,41 @@ class Lookup:
         `True` if the deletion succeeded; otherwise `False`
         """
 
-        target_bucket:List[dict]  = self.__get_bucket(key)
-        n =  len(target_bucket)
+        # [{'key': 5, 'value': 'a'}, {'key': 11, 'value': 'a'}, {'key': 3, 'value': 'a'}, {'key': 10, 'value': 'a'}, {'key': 4, 'value': 'a'}]
 
-        if n == 0:
+        # [{'key': 5, 'value': 'a'}, {'key': 11, 'value': 'a'}, {'key': 3, 'value': 'a'},        __None               , {'key': 4, 'value': 'a'}]
+
+        collision_count = 0
+        home_location = self.__hash(key,collision_count)
+        data:None|Data = self.buckets[home_location]
+
+        if data is None:
             return False
-        if n ==1:
-            del target_bucket[0]
+        
+        if data is not None and data.key == key and data.is_deleted:
+            return False
+        
+        if data is not None and data.key == key and data.is_deleted is False:
+            data.is_deleted = True
             self.size-=1
             return True
-        index_to_remove = None
-        for i, item in enumerate(target_bucket):
-            if item["key"] == key:
-                index_to_remove = i
-                break
-        del target_bucket [index_to_remove]
-
-        self.size-=1
-
-        return True
-
-    def get (self, key:Any)-> Any | None:
-        """
-        Gets a value by a specified key.
-
-        Returns
-        -------
-        `None` if the element does not exist; Otherwise the value is returned
-        """
-        target_bucket:List[dict]  = self.__get_bucket(key)
-        n =  len(target_bucket)
-        if n == 0:
-            return None
-        if n ==1:
-            return target_bucket[0]
         
-        # loop until we find element with key
-        for item in target_bucket:
-            if item["key"] == key:
-                return item["value"]
-        
-        return None
+        delta_location = None
+        collision_count+=1
+
+        # start probing!
+        # when load factor approaches 1 or is 1 search is O(n)
+
+        while home_location != delta_location:
+            delta_location = self.__hash(key,collision_count)
+            data:None|Data = self.buckets[delta_location]
+            if data is None:
+                return False
+            if data is not None and data.key == key and data.is_deleted is False:
+                data.is_deleted = True
+                self.size-=1
+                return True
+            collision_count+=1
+
+        return False
 
--- main.py
+++ main.py
@@ -3,27 +3,45 @@ from lookup import Lookup
 
 if __name__ == "__main__":
     
-    lookup  = Lookup()
+    lookup  = Lookup(length=5)
 
-    lookup.insert("foo",55)
+    lookup.insert(5,"a")
+    print(lookup.buckets)
     assert lookup.size == 1
 
-    lookup.insert("foo",65)
-    assert lookup.size == 1
-
-    delete_result = lookup.delete("foo")
-    assert lookup.size == 0
-    assert delete_result is True
-
-    delete_result = lookup.delete("foo")
-    assert delete_result is False
-
-    get_result = lookup.get("foo")
-    assert get_result is None
-
-    lookup.insert("foo",55)
-    lookup.insert("faa",55)
+    lookup.insert(4,"a")
+    print(lookup.buckets)
     assert lookup.size == 2
-    get_result = lookup.get("faa")
-    assert get_result is not None
+
+    lookup.insert(10,"a")   
+    print(lookup.buckets)
+    assert lookup.size == 3
+
+    lookup.insert(11,"a")   
+    print(lookup.buckets)
+    assert lookup.size == 4
+
+    lookup.insert(3,"a")   
+    print(lookup.buckets)
+    assert lookup.size == 5
+
+    lookup.insert(0,"a")   
+    print(lookup.buckets)
+    assert lookup.size == 5
+
+    r = lookup.search(5)
+    assert r != None
+
+    r = lookup.search(25)
+    assert r is None
+
+    r = lookup.delete(25)
+    assert r is False
+
+    r = lookup.delete(10)
+    assert r is True
+    print(lookup.buckets)
+    r = lookup.search(3)
+    assert r != None
+
 
